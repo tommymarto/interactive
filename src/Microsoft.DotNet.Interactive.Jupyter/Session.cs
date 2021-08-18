cb8c2c2f4a2632403fd47ed8c70bb3bbd69d0ea1
Tommaso Martorella <tom.martorella@gmail.com>
2021-08-18 13:54:54 +0200

    first steps kernel start

--- src/Microsoft.DotNet.Interactive.Jupyter/Session.cs
+++ src/Microsoft.DotNet.Interactive.Jupyter/Session.cs
@@ -3,9 +3,15 @@
 
 using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.DotNet.Interactive.Events;
+using Microsoft.DotNet.Interactive.Formatting;
 using Microsoft.DotNet.Interactive.Jupyter.Protocol;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.DotNet.Interactive.Jupyter
@@ -33,50 +39,119 @@ namespace Microsoft.DotNet.Interactive.Jupyter
 
     public class JupyterKernelProxy : Kernel, IKernelCommandHandler<SubmitCode>
     {
-        private readonly JupyterKernel _kernel;
+        private readonly JupyterKernelSession _kernelSession;
+        private readonly object _clientLock = new();
         private JupyterClient _client = null;
-        private Dictionary<string, TaskCompletionSource> _pendingRequests = new();
+        private Dictionary<string, CommandForwardingContext> _inflightCommands = new();
 
-        public JupyterKernelProxy(JupyterKernel kernel) : base(kernel.Name)
+        public JupyterKernelProxy(JupyterKernelSession kernel) : base(kernel.Name)
         {
-            _kernel = kernel;
-            _client.Subscribe(HandleResponse);
+            _kernelSession = kernel;
         }
 
         private void HandleResponse((JupyterChannel channel, ZMQ.Message message) e)
         {
-            //PublishEvent(new DisplayedValueProduced()
             var token = e.message.MetaData["commandToken"] as string;
-            var completionSource = _pendingRequests[token];
+            var forwardingContext = _inflightCommands[token];
             switch (e.message.Content)
             {
                 case ExecuteReplyOk:
-                    _pendingRequests.Remove(token);
-                    completionSource.TrySetResult();
+                    _inflightCommands.Remove(token);
+                    forwardingContext.CompletionSource.TrySetResult();
                     break;
                 case ExecuteReplyError reply:
-                    _pendingRequests.Remove(token);
-                    completionSource.TrySetException(new Exception(reply.EValue));
+                    _inflightCommands.Remove(token);
+                    forwardingContext.CompletionSource.TrySetException(new Exception(reply.EValue));
                     break;
                 default:
-                    throw new InvalidOperationException($"{e.message.Header.MessageType} is not supported.");
+                    TranslateAndForward(e.channel, e.message, forwardingContext.Context, forwardingContext.Command);
+                    break;
             }
+        }
+
+        private IReadOnlyCollection<FormattedValue> ToFormattedValues(IReadOnlyDictionary<string, object> data)
+        {
+            return data.Select(entry => new FormattedValue(entry.Key, entry.Value.ToString())).ToArray();
+        }
+
+        private void TranslateAndForward(JupyterChannel channel, ZMQ.Message message, KernelInvocationContext context, KernelCommand command)
+        {
+            switch(message.Content)
+            {
+                case DisplayData displayData:
+                    var displayedValueProduced = new DisplayedValueProduced(null, command, formattedValues: ToFormattedValues(displayData.Data));
+                    context.Publish(displayedValueProduced);
+                    break;
+                case UpdateDisplayData updateDisplayData:
+                    var valueId = GetValueId(updateDisplayData.Transient);
+                    var displayedValueUpdated = new DisplayedValueUpdated(null, valueId, command, formattedValues: ToFormattedValues(updateDisplayData.Data));
+                    context.Publish(displayedValueUpdated);
+                    break;
+                case ExecuteResult executeResult:
+                    var returnValueProduced = new ReturnValueProduced(null, command, formattedValues: ToFormattedValues(executeResult.Data));
+                    context.Publish(returnValueProduced);
+                    break;
+                case Stream stdout when stdout.Name == Stream.StandardOutput:
+                    var standardOutputValueProduced = new StandardOutputValueProduced(command, new[] { new FormattedValue(PlainTextFormatter.MimeType, stdout.Text ?? string.Empty) });
+                    context.Publish(standardOutputValueProduced);
+                    break;
+                case Stream stderr when stderr.Name == Stream.StandardError:
+                    var standardErrorValueProduced = new StandardErrorValueProduced(command, new[] { new FormattedValue(PlainTextFormatter.MimeType, stderr.Text ?? string.Empty) });
+                    context.Publish(standardErrorValueProduced);
+                    break;
+                case ExecuteInput when command is not SubmitCode:
+                    throw new InvalidOperationException($"received {message.Header.MessageType} while current command is {command.GetType().Name}.");
+                case ExecuteInput when command is SubmitCode:
+                    break;
+                default:
+                    throw new InvalidOperationException($"{message.Header.MessageType} is not supported.");
+            }
+        }
+
+        private string GetValueId(IReadOnlyDictionary<string, object> transient)
+        {
+            if(transient.TryGetValue("display_id", out var displayId))
+            {
+                return displayId is null ? Guid.NewGuid().ToString() : displayId.ToString();
+            }
+            return Guid.NewGuid().ToString();
         }
 
         public Task HandleAsync(SubmitCode command, KernelInvocationContext context)
         {
+            EnsureClient(context.CancellationToken);
+
             var request = new ExecuteRequest(command.Code);
             var requestMessage = _client.CreateMessage(request, command.GetToken());
             TaskCompletionSource taskCompletionSource = new();
-            _pendingRequests[command.GetToken()] = taskCompletionSource;
+            _inflightCommands[command.GetToken()] = new(context, command, taskCompletionSource);
             _client.Send(JupyterChannel.Shell, requestMessage);
             return taskCompletionSource.Task;
         }
+
+        private void EnsureClient(System.Threading.CancellationToken cancellationToken)
+        {
+            lock (_clientLock)
+            {
+                if(_client is null)
+                {
+                    var client = new JupyterClient(_kernelSession);
+                    _client = client;
+                    RegisterForDisposal(_client.Subscribe(HandleResponse));
+                    RegisterForDisposal(_client);
+                    RegisterForDisposal(_kernelSession);
+                    _kernelSession.StartAsync(cancellationToken).Wait(cancellationToken);
+                    _client.StartAsync(cancellationToken);
+                }
+            }
+        }
+
+        private record CommandForwardingContext(KernelInvocationContext Context, KernelCommand Command, TaskCompletionSource CompletionSource);
     }
 
-    public class JupyterKernel
+    public class JupyterKernelSession : IDisposable
     {
-        public JupyterKernel(string name, Session session, ConnectionInformation connectionInformation)
+        public JupyterKernelSession(string name, Session session)
         {
             if (string.IsNullOrWhiteSpace(name))
             {
@@ -84,11 +159,45 @@ namespace Microsoft.DotNet.Interactive.Jupyter
             }
             Name = name;
             Session = session ?? throw new ArgumentNullException(nameof(session));
-            ConnectionInformation = connectionInformation ?? throw new ArgumentNullException(nameof(connectionInformation));
         }
 
         public string Name { get; }
         public Session Session { get; }
-        public ConnectionInformation ConnectionInformation { get; }
+        public ConnectionInformation ConnectionInformation { get; private set; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var connectionFile = Path.Combine(
+                Path.GetTempPath(),
+                "jupyter",
+                "runtime",
+                $"{Session.Id}.json");
+
+            var connectionInformationTask = GetConnectionInformationAsync(connectionFile, cancellationToken);
+
+            // TODO: start process
+
+            ConnectionInformation = await connectionInformationTask;
+
+            throw new NotImplementedException();
+        }
+
+        private async Task<ConnectionInformation> GetConnectionInformationAsync(string connectionFile, CancellationToken cancellationToken)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var timeout = TimeSpan.FromSeconds(10);
+            while (!File.Exists(connectionFile) && stopWatch.Elapsed < timeout)
+            {
+                await Task.Delay(250);
+            }
+            await Task.Delay(500);
+            var content = await File.ReadAllTextAsync(connectionFile, cancellationToken);
+            return JsonSerializer.Deserialize<ConnectionInformation>(content);
+        }
     }
 }
